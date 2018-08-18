3eda16d985aa9b0c81ca86ff47eb417991da78fb
DM <944095635@qq.com>
2018-08-18 23:37:17 +0800

    2.0.0.8

--- DMSkin.WPF/API/ViewModelBase.cs
+++ DMSkin.WPF/API/ViewModelBase.cs
@@ -12,5 +12,23 @@ namespace DMSkin
                 PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
+
+
+        #region 是否正在加载
+        private bool isLoad;
+
+        /// <summary>
+        /// 是否加载
+        /// </summary>
+        public bool IsLoad
+        {
+            get { return isLoad; }
+            set
+            {
+                isLoad = value;
+                OnPropertyChanged("IsLoad");
+            }
+        } 
+        #endregion
     }
 }
--- DMSkin.WPF/Controls/DMCheckBox.cs
+++ DMSkin.WPF/Controls/DMCheckBox.cs
@@ -17,5 +17,19 @@ namespace DMSkin.WPF.Controls
         }
         public static readonly DependencyProperty IconColorProperty =
             DependencyProperty.Register("IconColor", typeof(SolidColorBrush), typeof(DMCheckBox), new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 255,255,255))));
+
+
+        /// <summary>
+        /// 是否显示内容
+        /// </summary>
+        public bool IsShowContent
+        {
+            get { return (bool)GetValue(IsShowContentProperty); }
+            set { SetValue(IsShowContentProperty, value); }
+        }
+        public static readonly DependencyProperty IsShowContentProperty =
+            DependencyProperty.Register("IsShowContent", typeof(bool), typeof(DMCheckBox), new PropertyMetadata(true));
+
+
     }
 }
