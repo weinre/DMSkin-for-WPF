d72622468f40bd8ab8e2ec2809b3bd1bcd587a93
Dream.Machine <944095635@qq.com>
2018-11-30 17:20:52 +0800

    2.5.1.2

---
+++ DMSkin.Core/Common/UINT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMSkin.Core.Common
+{
+    public class UINT
+    {
+        /// <summary>
+        /// 将大小转换为KB
+        /// </summary>
+        public string UINTFormat(double size)
+        {
+            string[] units = new String[] { "B", "KB", "MB", "GB", "TB", "PB" };
+            double mod = 1024.0;
+            int i = 0;
+            while (size >= mod)
+            {
+                size /= mod;
+                i++;
+            }
+            return Math.Round(size, 2) + units[i];
+        }
+    }
+}
